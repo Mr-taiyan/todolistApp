c598d8e36d973bab722ff613b25adbc341ab63af
chen <cty9388@hotmail.com>
2019-09-20 10:29:24 +0800

    update boxes.js

--- src/Boxes.js
+++ src/Boxes.js
@@ -1,23 +1,38 @@
 import React from "react";
 
-const Boxes = ({ Arr }) => {
+const Boxes = ({ Arr, windowInformation }) => {
   let column = Arr.length ? Arr.length : 0;
-  column = column > 4 ? column : 4;
+  column =
+    column > windowInformation.defaultColumn
+      ? column
+      : windowInformation.defaultColumn;
   let boxes = [];
   for (let i = 0; i < column; i++) {
     for (let j = 0; j < 24; j++) {
       boxes.push(
-        <div className={i % 2 === 1 ? "blockwhite" : "blockgray"}></div>
+        <div
+          className={i % 2 === 1 ? "blockwhite" : "blockgray"}
+          style={{
+            width: windowInformation.unitWidth,
+            height: windowInformation.unitHeight
+          }}
+        ></div>
       );
     }
   }
 
   const wrap = (
     <div className="table">
-      <div className="title" style={{ width: column * 144 }}>
+      <div
+        className="title"
+        style={{ width: column * windowInformation.unitWidth }}
+      >
         日程内容
       </div>
-      <div className="flexbox" style={{ width: column * 144 }}>
+      <div
+        className="flexbox"
+        style={{ width: column * windowInformation.unitWidth }}
+      >
         {boxes}
       </div>
     </div>
